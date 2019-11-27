60cce2282f25da035b2f4a1c33e40cc258eada3e
Luca Terrazzan <luca.terraz@gmail.com>
2019-11-27 14:25:19 +0100

    added repo checks

--- src/git-manager.ts
+++ src/git-manager.ts
@@ -4,9 +4,20 @@ import simplegit, { SimpleGit } from "simple-git/promise";
 export class GitManager {
 
   private readonly git: SimpleGit;
+  private repo: string;
 
   constructor(private readonly folder: string) {
     this.git = simplegit(folder);
+    this.repo = '';
+  }
+
+  public async init() {
+    if (!this.git.checkIsRepo()) {
+      console.error(`Folder ${this.folder} is not a git repo!`);
+      throw new Error(`Folder ${this.folder} is not a git repo!`);
+    }
+
+    this.repo = (await this.git.getRemotes(true))[0].refs.fetch;
   }
 
   public async getStatus() {
@@ -19,6 +30,15 @@ export class GitManager {
 
   public async checkoutReleaseBranchLocally(branch: string) {
     return await this.git.checkoutLocalBranch(branch);
+  }
+
+  public async hardReset() {
+    try{
+      this.git.reset('hard');
+    } catch (e) {
+      console.error(`In repo ${}`);
+
+    }
   }
 
 }
