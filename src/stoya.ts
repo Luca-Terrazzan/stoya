9032a3bd47f340790f19f542826c5c8d876ada2e
Luca Terrazzan <luca.terraz@gmail.com>
2019-11-28 12:14:14 +0100

    removed unused options

--- src/stoya.ts
+++ src/stoya.ts
@@ -1,34 +1,9 @@
 import 'colors';
 
 import * as config from '../config.json';
-import { description, name, version } from '../package.json';
 import { GitManager } from './git-manager';
 
 async function main() {
-
-  const options = process.argv.slice(2);
-
-  if (options.includes('--version')) {
-    console.log(`${name} version ${version}`);
-
-    process.exit(0);
-  }
-
-  if (options.includes('--help')) {
-    const helpMessage = `
-      ${name} version ${version}
-      ${description}
-
-      Usages:
-
-      --help Show this screen
-      --version Show currently installed version
-    `;
-
-    console.log(helpMessage);
-
-    process.exit(0);
-  }
 
   // Execute script
   console.log('\n 💣  Starting the creation of release branches 💣\n'.underline.bold);
