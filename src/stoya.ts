3f46207c0c6516f4dbaad4d41c0be5969c88ae5f
Luca Terrazzan <luca.terraz@gmail.com>
2019-11-28 12:00:00 +0100

    added final success message

--- src/stoya.ts
+++ src/stoya.ts
@@ -3,6 +3,7 @@ import 'colors';
 import * as config from '../config.json';
 import { description, name, version } from '../package.json';
 import { GitManager } from './git-manager';
+import { release } from 'os';
 
 async function main() {
 
@@ -33,18 +34,25 @@ async function main() {
   // Execute script
   console.log('\n 💣  Starting the creation of release branches 💣\n'.underline.bold);
 
+  const releases: Array<Promise<void>> = [];
+
   for (const repo of config.repositories) {
     console.log(`🙏  Creating release for repository ${repo.bold}`.green);
 
     const gitMngr = new GitManager(repo);
 
     // Launch all releases in parallel => no async/await here please!
-    gitMngr.createRelease(config.branches.master, config.branches.release, config.branches.development)
-    .catch((err) => {
-      console.error(`🚨🚨🚨  An error occurred while trying to create a release for repo ${repo.bold} 🚨🚨🚨
-        Please perform a manual check on this repo!`.red);
-    });
+
+    releases.push(gitMngr.createRelease(config.branches.master, config.branches.release, config.branches.development)
+      .catch((err) => {
+        console.error(`🚨 🚨 🚨  An error occurred while trying to create a release for repo ${repo.bold} 🚨 🚨 🚨
+          Please perform a manual check on this repo!`.red);
+      }),
+    );
   }
+
+  await Promise.all(releases);
+  console.log(`\n\n🎊 🎊 🎊  Release Process Completed 🎊 🎊 🎊 `.green.bold);
 
 }
 
