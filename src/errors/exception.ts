13999397b5297a72f034838298998823de5850f2
Luca Terrazzan <luca.terraz@gmail.com>
2020-03-03 16:19:56 +0100

    chore(comments)

--- src/errors/exception.ts
+++ src/errors/exception.ts
@@ -6,8 +6,8 @@ export class Exception extends Error {
   constructor(message?: string) {
     super(message);
 
-    // This is needed to dynamycally set the error name
-    // Otherwise it's always 'Error'
+    // This is needed to dynamically set the error name
+    // Otherwise it's always 'Error' for typescript versions < 3.7
     this.name = this.constructor.name;
   }
 }
